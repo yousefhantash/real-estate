b04c69a3b5aa6a2eba6bbdbf61c177ee6c2a9c06
yousefhantash <yousef.pizza17@gmail.com>
2024-08-03 16:18:59 +0300

    addition update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import WarppedFindP from "./components/FindP";
-import WarppedNavbar from "./components/Navbar";
-import WarppedPlatform from "./components/Platform";
 import Home from './views/Home';
 
 
@@ -11,24 +7,8 @@ function App() {
   return (
     <div className="bg-[#F5F2EF]">
 
-  
-  <WarppedNavbar/>
-
-  <WarppedFindP/>
-  <WarppedPlatform/> 
-
-  <Home/>
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router> 
-
-  
-
-
-
+        <Home />
+    
      </div>
     
   );
