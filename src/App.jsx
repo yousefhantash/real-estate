b151a735417278a91a262b3e8585527b8f764546
ahmad <ahmad.im.iseed@gmail.com>
2024-08-06 14:43:50 +0300

    i made a new file  called PassDataRoute in views

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Details from './components/Detailspage/Details';
 import Home from './views/Home';
 import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import PassDataRoute from './views/PassDataRoute';
 
 
 
@@ -9,12 +10,7 @@ function App() {
   return (
 
     <div className="bg-[#F5F2EF]">
-          <BrowserRouter >
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/properties/post/:name" element={<Details />} />
-    </Routes>
-    </BrowserRouter>
+<PassDataRoute/>
     </div>
     
 
