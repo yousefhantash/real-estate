6db3e99b095cbb4e8a43417560b60c757fc181a2
ahmad <ahmad.im.iseed@gmail.com>
2024-08-18 15:27:46 +0300

    .

---
+++ src/AdminPage/Admin.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+function Admin() {
+ return (
+ <>
+ <div className='container mx-auto'>
+
+ <div className="flex items-start ml-1.5 xl   justify-center my-[2em]">
+    <h1 className="text-[#CF991E] sm:text-[23px] md:text-[55px] text-[33px] font-bold "> ME</h1>
+    <h1 className="text-[#0056B3] sm:text-[32px] md:text-[60px] text-[36px] tracking-[4.00px] mt-1 font-bold _">Palestine</h1>
+    </div>
+    <div className='shadow-xl '>
+
+
+
+
+
+    <div  className="grid md:grid-cols-8 sm:grid-cols-1 px-3 py-3 border-r-8 border-custom-blue items-center my-[2em]">
+     <img src="" alt="" />
+
+
+
+
+    </div>
+      </div>
+
+    </div>
+
+ </>
+  )
+}
+
+export default Admin
