d16cd19caac9c46f1a38b0111805bb0bbfac5b87
ahmad <ahmad.im.iseed@gmail.com>
2024-08-30 00:05:08 +0300

    i made search functionalityy for the links in the footer and the navbar in depends on the propertyy type and the oofer type and the the city and ive made the links changeable dependes on what u have choose in the search bar and i get the data from the api and added it in the recently added proprties

--- src/components/RealEstateOverView/Buying.jsx
+++ src/components/RealEstateOverView/Buying.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Ow1Search from "../../images/Recentlyaddedproperties/Home-OverView/OW1search.png";
 import "./OverView.css";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { LandContext } from '../../views/PassDataRoute';
 
 function Buying() {
+  const data = useContext(LandContext);
+  const navigate=useNavigate()
+    
+  
+  const searched = () => {
+      const searchedData = data.filter((item) => {
+        return (
+          (item.offerType === "For Sale")
+        );
+        
+      }
+    ); 
+    setTimeout(() => {
+      navigate(`/searched/${"ForSale"}`,{state:searchedData})
+
+    }, 0);
+
+    console.log(searchedData); 
+  };
+
   return (
        <div className='bg-white shadow p-6 flex flex-col items-center hover:scale-110 duration-300'>
     <img src={Ow1Search} alt="Buying a property" className='mx-auto'/>
@@ -11,11 +32,10 @@ function Buying() {
     <p className=' mx-14 pb-8'>
       Find the property of the future with a quick search engine experience and a simple interface that you won't find anywhere else.
     </p>
-    <NavLink  to="">
-<button className='my-14  px-10 py-2 bg-custom-blue rounded text-white hover:bg-blue-400 duration-300'>
+<button onClick={searched} className='my-14  px-10 py-2 bg-custom-blue rounded text-white hover:bg-blue-400 duration-300'>
       Search for a Property
     </button>
-    </NavLink >
+  
   </div>
 
   )
--- src/components/RealEstateOverView/Renting.jsx
+++ src/components/RealEstateOverView/Renting.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./OverView.css";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import Ow3Rent from "../../images/Recentlyaddedproperties/Home-OverView/OW3rent.png";
+import { LandContext } from '../../views/PassDataRoute';
 
 function Renting() {
+
+  const data = useContext(LandContext);
+  const navigate=useNavigate()
+    
+  
+  const searched = () => {
+      const searchedData = data.filter((item) => {
+        return (
+          (item.offerType === "For Rent")
+        );
+        
+      }
+    ); 
+    setTimeout(() => {
+      navigate(`/searched/${"ForRent"}`,{state:searchedData})
+
+    }, 0);
+
+    console.log(searchedData); 
+  };
+
+
+
+
   return (
     <div className='bg-white shadow p-6 flex flex-col items-center hover:scale-110 duration-300 justify'>
     <img src={Ow3Rent} alt="Rent a property" className='mx-auto'/>
@@ -15,11 +40,9 @@ faster way, one place for everyone in
 which to combine the owner and the
 tenant.
     </p>
-        <NavLink  to="">
-<button className="px-10 py-2 bg-custom-blue rounded text-white hover:bg-blue-400 transition duration-300">
+<button onClick={searched} className="px-10 py-2 bg-custom-blue rounded text-white hover:bg-blue-400 transition duration-300">
   Find a property to rent
 </button>
-</NavLink >
   </div>
   )
 }
